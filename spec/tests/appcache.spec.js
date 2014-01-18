5976175c9b8a75fcab47a84b315d311b455be6e4
olav <olav@bjorkoy.com>
2014-01-18 14:20:42 +0100

    Use config.timestamp in appcache generation

--- spec/tests/appcache.spec.js
+++ spec/tests/appcache.spec.js
@@ -9,33 +9,28 @@ exports.canCreateAppCacheFile = function (test) {
     pacbot.config({
         appdir: 'spec/cases/cache/content',
         pubdir: 'spec/out/cache',
-        config: 'spec/cases/cache/pacbot.js'
+        config: 'spec/cases/cache/pacbot.js',
+        timestamp: false
     });
     pacbot.build(function () {
         var content = fss.readFile('spec/out/cache/cache.appcache');
-        contains(test, content, 'CACHE MANIFEST\n');
-        contains(test, content, '\n\nCACHE:\n');
-        contains(test, content, '\n\nNETWORK:\n*\n');
-        contains(test, content, '\n/img/1.png#v=');
-        contains(test, content, '\n/img/a/2.png#v=');
-        contains(test, content, '\n/img/a/3.png#v=');
-        contains(test, content, '\n/img/b/4.png#v=');
-        contains(test, content, '\n/css/1.css#v=');
-        contains(test, content, '\nprefer-online');
+        var solution = fss.readFile('spec/out/cache/solution.appcache');
+        test.equal(content, solution);
         test.done();
     });
 };
 
-exports.canHonorRootPrefix = function (test) {
+exports.canHonorRootAndTimestamp = function (test) {
     pacbot.config({
         appdir: 'spec/cases/cache/content',
         pubdir: 'spec/out/cache',
         config: 'spec/cases/cache/pacbot.js',
-        root: 'foo/bar/'
+        root: 'foo/bar/',
+        timestamp: true
     });
     pacbot.build(function () {
         var content = fss.readFile('spec/out/cache/cache.appcache');
-        contains(test, content, 'CACHE MANIFEST\n');
+        contains(test, content, 'CACHE MANIFEST\n# Time: ');
         contains(test, content, '\nfoo/bar/img/1.png#v=');
         contains(test, content, '\nfoo/bar/img/a/2.png#v=');
         contains(test, content, '\nfoo/bar/css/1.css#v=');
